e131f7b87416d1b466282daf9a515b191e38d4af
natalijadav <natalylab@gmail.com>
2021-03-12 21:13:59 +0100

    l

--- index.php
+++ index.php
@@ -7,7 +7,30 @@
         <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
         <button>About us</button>
     </header>
+    <span></span>
 
+<!-- headings -->
+    <section>
+    <article>
+            <h2>01</h2>
+        </article>
+        <article>
+            <h3>About us</h3>
+            <h2><span> We can</span>HELP Your Business GROW</h2>
+            <h4>Get your businesb 4 easy steps</h4>   
+        </article>       
+    </section>
 <?php include "partials/footer.php" ?>
+<!-- about -->
+    <section class="about">
+        <article></article>
+        <article>
+            <h2>Everything</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae tempora aut earum officiis dolore vero repellat ea at voluptatem veritatis. Dicta accusamus facere necessitatibus alias porro eos autem facilis, voluptate iure incidunt delectus sequi debitis, voluptatibus quis nihil! Et, perferendis!
+            </p>
+        </article>
+    </section>
+
+
 
     
