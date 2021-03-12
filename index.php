e6b4de17210e6ff8bcf80300ab897788cde9bff2
natalijadav <natalylab@gmail.com>
2021-03-12 21:50:04 +0100

    l

--- index.php
+++ index.php
@@ -7,65 +7,82 @@
         <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
         <button>About us</button>
     </header>
-    <span></span>
 
-<!-- headings -->
-    <section>
-    <article>
+    <!-- headings -->
+    <section class="container headings">
+        <article>
             <h2>01</h2>
         </article>
         <article>
             <h3>About us</h3>
-            <h2><span> We can</span>HELP Your Business GROW</h2>
-            <h4>Get your businesb 4 easy steps</h4>   
-        </article>       
+            <h2><span>WE CAN</span> HELP YOUR BUSINESS GROW</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
     </section>
+
+    <!-- about -->
     <section class="about">
         <article>
             <img src="img/about.jpg" alt="">
         </article>
         <article>
-            <h2>Everything</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae tempora aut earum officiis dolore vero repellat ea at voluptatem veritatis. Dicta accusamus facere necessitatibus alias porro eos autem facilis, voluptate iure incidunt delectus sequi debitis, voluptatibus quis nihil! Et, perferendis!
-            </p>
+            <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eos incidunt aperiam, dolorem. Iste voluptatibus repellendus omnis, perferendis beatae quasi dicta possimus similique ad, facilis nemo! Velit molestias blanditiis deleniti voluptatum cupiditate ullam alias magnam, soluta doloremque distinctio optio repellendus voluptas voluptatibus nesciunt ab modi, facilis possimus incidunt iste, ad?</p>
         </article>
     </section>
-<!-- services -->
-    <section>
+
+    <!-- headings -->
+    <section class="container headings">
         <article>
-            <i class="fas fa-chart-bar"></i>
-            <h3>marketing</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium rerum alias nam omnis, dolore veritatis quibusdam? Voluptatem suscipit voluptatibus deserunt, unde reiciendis asperiores quae modi sed et, doloremque, quibusdam iusto ea! Repellendus laboriosam est minima officia laudantium dignissimos eos ex!</p>
+            <h2>02</h2>
+        </article>
+        <article>
+            <h3>Services</h3>
+            <h2><span>HOW TO PROVIDE</span> TOOLS THAT HELP ANYONE</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
+
+    <!-- services -->
+    <section class="services container">
+        <article>
+            <i class="far fa-chart-bar"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-bullseye"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-map-signs"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
         </article>
         <article>
             <i class="fas fa-filter"></i>
-            <h3>marketing</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium rerum alias nam omnis, dolore veritatis quibusdam? Voluptatem suscipit voluptatibus deserunt, unde reiciendis asperiores quae modi sed et, doloremque, quibusdam iusto ea! Repellendus laboriosam est minima officia laudantium dignissimos eos ex!</p>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
         </article>
         <article>
-            <i class="fas fa-hands-holding-usd"></i>
-            <h3>marketing</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium rerum alias nam omnis, dolore veritatis quibusdam? Voluptatem suscipit voluptatibus deserunt, unde reiciendis asperiores quae modi sed et, doloremque, quibusdam iusto ea! Repellendus laboriosam est minima officia laudantium dignissimos eos ex!</p>
+            <i class="fas fa-hand-holding-usd"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
         </article>
         <article>
-            <i class="far fa-bullhorn"></i>
-            <h3>marketing</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium rerum alias nam omnis, dolore veritatis quibusdam? Voluptatem suscipit voluptatibus deserunt, unde reiciendis asperiores quae modi sed et, doloremque, quibusdam iusto ea! Repellendus laboriosam est minima officia laudantium dignissimos eos ex!</p>
-        </article>
-        <article>
-            <i class="far fa-bullseye"></i>
-            <h3>marketing</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium rerum alias nam omnis, dolore veritatis quibusdam? Voluptatem suscipit voluptatibus deserunt, unde reiciendis asperiores quae modi sed et, doloremque, quibusdam iusto ea! Repellendus laboriosam est minima officia laudantium dignissimos eos ex!</p>
-        </article>
-        <article>
-            <i class="far fa chart base_convert"></i>
-            <h3>marketing</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium rerum alias nam omnis, dolore veritatis quibusdam? Voluptatem suscipit voluptatibus deserunt, unde reiciendis asperiores quae modi sed et, doloremque, quibusdam iusto ea! Repellendus laboriosam est minima officia laudantium dignissimos eos ex!</p>
+            <i class="fas fa-bullhorn"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
         </article>
     </section>
+
 <?php include "partials/footer.php" ?>
-
-    
-
 
     
